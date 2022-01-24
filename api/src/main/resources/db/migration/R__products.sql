ce5b7003a131922f791070e46828b6f1b34d77b0
Allan Pereira <allan.pereira.cunha@gmail.com>
2022-01-24 01:38:42 -0300

    Move migration to also populate development database

---
+++ api/src/main/resources/db/migration/R__products.sql
@@ -0,0 +1,3 @@
+INSERT INTO `products`(`name`, `price`) VALUES('Apple MacBook Air 8GB RAM 256GB SSD', 89999);
+INSERT INTO `products`(`name`, `price`) VALUES('Apple iPhone 13 128GB', 69999);
+INSERT INTO `products`(`name`, `price`) VALUES('Apple Watch Series 7 (GPS + Cellular) 41mm', 49999);
--- api/src/test/resources/db/migration/R__products.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `codebrain-challenge-tests`.`products`(`name`, `price`) VALUES('Apple MacBook Air 8GB RAM 256GB SSD', 89999);
-INSERT INTO `codebrain-challenge-tests`.`products`(`name`, `price`) VALUES('Apple iPhone 13 128GB', 69999);
-INSERT INTO `codebrain-challenge-tests`.`products`(`name`, `price`) VALUES('Apple Watch Series 7 (GPS + Cellular) 41mm', 49999);
