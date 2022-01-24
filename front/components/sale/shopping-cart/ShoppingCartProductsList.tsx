0a766e2226be0f3a9700aec7c60bdf8ac2d032f5
Allan Pereira <allan.pereira.cunha@gmail.com>
2022-01-24 15:53:32 -0300

    Fix minor bugs in layout when running in mobile devices

--- front/components/sale/shopping-cart/ShoppingCartProductsList.tsx
+++ front/components/sale/shopping-cart/ShoppingCartProductsList.tsx
@@ -36,18 +36,18 @@ export default function ShoppingCartProductsList() {
         sale.items.map((item, idx) => {
           return <Box rounded='md' bg='white' mt={2} mb={2}>
             <Grid templateColumns='repeat(12, 1fr)' spacing={10}>
-              <GridItem colSpan={8} >
+              <GridItem colSpan={[12, 8, 8, 8, 8]} >
                 <Heading as='h5' size='sm'>
                   {item.product.name}
                 </Heading>
                 <Text fontSize='md'>{item.quantity}x ${item.product.price / 100}</Text>
               </GridItem>
-              <GridItem colSpan={2} >
+              <GridItem colSpan={[6, 2, 2, 2, 2]} >
                 <Heading as='h4' size='md'>
                   {CurrencyFormatter(item.product.price * item.quantity)}
                 </Heading>
               </GridItem>
-              <GridItem colSpan={2} >
+              <GridItem  colSpan={[6, 2, 2, 2, 2]} >
                 <Flex>
                   <Spacer />
                   <Box>
