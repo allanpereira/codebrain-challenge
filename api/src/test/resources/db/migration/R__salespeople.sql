498198ddf6257f913a689bdd80a9f09ca9b0d962
Allan Pereira <allan.pereira.cunha@gmail.com>
2022-01-24 22:19:15 -0300

    Execute salespeople migration in development environment

--- api/src/test/resources/db/migration/R__salespeople.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `codebrain-challenge-tests`.`salespeople`(`name`, `registration`) VALUES('John Doe', 'RG-000001');
-INSERT INTO `codebrain-challenge-tests`.`salespeople`(`name`, `registration`) VALUES('Jane Doe', 'RG-000002');
-INSERT INTO `codebrain-challenge-tests`.`salespeople`(`name`, `registration`) VALUES('Emma Lee', 'RG-000003');
